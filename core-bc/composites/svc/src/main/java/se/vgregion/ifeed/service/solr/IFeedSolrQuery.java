538281a483d8819e56439fb02c75cdd6b57dd859
aasplund@gmail.com <aasplund@gmail.com@9c261296-49e3-ae38-c888-6b3422c0b4e0>
2011-05-04 08:19:50 +0000

    Adding first edition of atom entry for ifeed metadata. Change service endpoints.

--- core-bc/composites/svc/src/main/java/se/vgregion/ifeed/service/solr/IFeedSolrQuery.java
+++ core-bc/composites/svc/src/main/java/se/vgregion/ifeed/service/solr/IFeedSolrQuery.java
@@ -2,7 +2,6 @@ package se.vgregion.ifeed.service.solr;
 
 import java.net.MalformedURLException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -12,65 +11,64 @@ import org.apache.solr.client.solrj.impl.BinaryRequestWriter;
 import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
 
-import se.vgregion.ifeed.types.FilterType;
 import se.vgregion.ifeed.types.IFeed;
 import se.vgregion.ifeed.types.IFeedFilter;
 
 
 public class IFeedSolrQuery extends SolrQuery {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private CommonsHttpSolrServer solrServer;
-	
-	/*public enum ORDER { desc, asc;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private CommonsHttpSolrServer solrServer;
+
+    /*public enum ORDER { desc, asc;
 		public ORDER reverse() {
 			return (this == asc) ? desc : asc;
 		}
 	}*/
 
-	
-	public void setSolrServer(CommonsHttpSolrServer solrServer) {
-		this.solrServer = solrServer;
-		this.solrServer.setRequestWriter(new BinaryRequestWriter());
-	}
-	public CommonsHttpSolrServer getSolrServer() {
-		return solrServer;
-	}
-	public QueryResponse query() throws MalformedURLException, SolrServerException {
-		return getSolrServer().query(this);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public ArrayList<Map<String, Object>> doFilterQuery() {
-		this.setSortField("revisiondate", SolrQuery.ORDER.desc);
-		ArrayList<Map<String, Object>> hits = null;
-		try {
-			hits = (ArrayList<Map<String, Object>>)this.query().getResults().clone();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SolrServerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return hits;
-	}
-	
-	public List<Map<String, Object>> getIFeedResults(IFeed iFeed) {
-		setQuery("*:test");
-		String[] solrFilters = new String[iFeed.getFilters().size()];
-		
-		int index = 0; 
-		for (IFeedFilter iFeedFilter : iFeed.getFilters()) {
-			solrFilters[index++] = iFeedFilter.getFilter().getFilterField() + ":" + iFeedFilter.getFilterQuery();
-		}
-		// Populate the query with the feed's filters
-        //setFilterQueries(solrFilters);
+
+    public void setSolrServer(CommonsHttpSolrServer solrServer) {
+        this.solrServer = solrServer;
+        this.solrServer.setRequestWriter(new BinaryRequestWriter());
+    }
+    public CommonsHttpSolrServer getSolrServer() {
+        return solrServer;
+    }
+    public QueryResponse query() throws MalformedURLException, SolrServerException {
+        return getSolrServer().query(this);
+    }
+
+    @SuppressWarnings("unchecked")
+    public ArrayList<Map<String, Object>> doFilterQuery() {
+        this.setSortField("revisiondate", SolrQuery.ORDER.desc);
+        ArrayList<Map<String, Object>> hits = null;
+        try {
+            hits = (ArrayList<Map<String, Object>>)this.query().getResults().clone();
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SolrServerException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return hits;
+    }
+
+    public List<Map<String, Object>> getIFeedResults(IFeed iFeed) {
+        setQuery("");
+        String[] solrFilters = new String[iFeed.getFilters().size()];
+
+        int index = 0;
+        for (IFeedFilter iFeedFilter : iFeed.getFilters()) {
+            solrFilters[index++] = iFeedFilter.getFilter().getFilterField() + ":" + iFeedFilter.getFilterQuery();
+        }
+        // Populate the query with the feed's filters
+        setFilterQueries(solrFilters);
 
         // Perform query
         return doFilterQuery();
-	}
+    }
 }

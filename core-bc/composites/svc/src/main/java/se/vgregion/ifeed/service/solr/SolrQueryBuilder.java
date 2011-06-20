6a3817df8a03850fddc056c8d0a9bd118a31511b
aasplund@gmail.com <aasplund@gmail.com@9c261296-49e3-ae38-c888-6b3422c0b4e0>
2011-06-20 09:14:22 +0000

    Solr Query escaping - ready

--- core-bc/composites/svc/src/main/java/se/vgregion/ifeed/service/solr/SolrQueryBuilder.java
+++ core-bc/composites/svc/src/main/java/se/vgregion/ifeed/service/solr/SolrQueryBuilder.java
@@ -8,18 +8,19 @@ public class SolrQueryBuilder {
     public static String createQuery(IFeedFilter iFeedFilter) {
         String query = "";
         Filter filter = iFeedFilter.getFilter();
+        String filterQuery = iFeedFilter.getFilterQuery();
         switch (filter.getMetadataType()) {
             case TEXT_FREE:
-                query  = filter.getFilterField() + ":" + iFeedFilter.getFilterQuery() + "*";
+                query  = filter.getFilterField() + ":" + SolrQueryEscaper.escape(filterQuery) + "*";
                 break;
             case TEXT_FIX:
-                query = filter.getFilterField() + ":" + iFeedFilter.getFilterQuery();
+                query = filter.getFilterField() + ":" + SolrQueryEscaper.escape(filterQuery);
                 break;
             case DATE:
                 if(filter.name().contains("FROM_DATE")) {
-                    query = filter.getFilterField() + ":[" + iFeedFilter.getFilterQuery() + " TO *]";
+                    query = filter.getFilterField() + ":[" + filterQuery + " TO *]";
                 } else if(filter.name().contains("TO_DATE")) {
-                    query = filter.getFilterField() + ":[* TO " + iFeedFilter.getFilterQuery() + "]";
+                    query = filter.getFilterField() + ":[* TO " + filterQuery + "]";
                 } else {
                     throw new RuntimeException("Unable to build query. Unknown filter date type found: " + filter.name());
                 }

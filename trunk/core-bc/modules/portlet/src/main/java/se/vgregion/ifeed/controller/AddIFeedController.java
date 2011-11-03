eba54a9156ac04092cfb2263b1cbc3309bd00179
claes.lundahl@gmail.com <claes.lundahl@gmail.com@9c261296-49e3-ae38-c888-6b3422c0b4e0>
2011-11-03 14:23:04 +0000

    Added right-management for iFeeds. Commented the button out in the gui.

--- trunk/core-bc/modules/portlet/src/main/java/se/vgregion/ifeed/controller/AddIFeedController.java
+++ trunk/core-bc/modules/portlet/src/main/java/se/vgregion/ifeed/controller/AddIFeedController.java
@@ -24,70 +24,86 @@ import org.springframework.web.portlet.bind.annotation.RenderMapping;
 import se.vgregion.ifeed.service.ifeed.IFeedService;
 import se.vgregion.ifeed.types.IFeed;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.service.ResourceLocalService;
+import com.liferay.portal.theme.ThemeDisplay;
+
 @Controller
 @RequestMapping("VIEW")
-@SessionAttributes({"ifeed"})
+@SessionAttributes({ "ifeed" })
 public class AddIFeedController {
-    private IFeedService iFeedService;
+	private IFeedService iFeedService;
+	private ResourceLocalService resourceLocalService;
 
-    @Autowired
-    public AddIFeedController(final IFeedService iFeedService) {
-        super();
-        this.iFeedService = iFeedService;
-    }
+	@Autowired
+	public AddIFeedController(final IFeedService iFeedService, final ResourceLocalService resourceLocalService) {
+		super();
+		this.iFeedService = iFeedService;
+		this.resourceLocalService = resourceLocalService;
+	}
 
-    @ModelAttribute("ifeed")
-    public IFeed getCommandObject() {
-        return new IFeed();
-    }
+	@ModelAttribute("ifeed")
+	public IFeed getCommandObject() {
+		return new IFeed();
+	}
 
-    @RenderMapping(params = {"view=showAddIFeedForm"})
-    public String showAddIFeedForm(final RenderResponse response) {
-        return "addIFeedForm";
-    }
+	@RenderMapping(params = { "view=showAddIFeedForm" })
+	public String showAddIFeedForm(final RenderResponse response) {
+		return "addIFeedForm";
+	}
 
-    @InitBinder("ifeed")
-    public void initBinder(final WebDataBinder binder) {
-        //TODO Add validators
-    }
+	@InitBinder("ifeed")
+	public void initBinder(final WebDataBinder binder) {
+		// TODO Add validators
+	}
 
-    // @ExceptionHandler({ Exception.class })
-    public String handleException() {
-        return "errorPage";
-    }
+	// @ExceptionHandler({ Exception.class })
+	public String handleException() {
+		return "errorPage";
+	}
 
-    @ActionMapping(params = {"action=createNewIFeed"})
-    public void createNewIFeed(final ActionResponse response,
-            final SessionStatus sessionStatus, final Model model) {
-        sessionStatus.setComplete();
-        model.addAttribute("ifeed", new IFeed());
-        response.setRenderParameter("view", "showAddIFeedForm");
-    }
+	@ActionMapping(params = { "action=createNewIFeed" })
+	public void createNewIFeed(final ActionResponse response, final SessionStatus sessionStatus, final Model model) {
+		sessionStatus.setComplete();
+		model.addAttribute("ifeed", new IFeed());
+		response.setRenderParameter("view", "showAddIFeedForm");
+	}
 
-    @ActionMapping(params = {"action=addIFeed"})
-    public void addIFeed(final Model model,
-            @Valid @ModelAttribute(value = "ifeed") IFeed iFeed,
-            final BindingResult bindingResult, final ActionRequest request,
-            final ActionResponse response) {
-        if (!bindingResult.hasErrors()) {
-            iFeed.setUserId(getRemoteUserId(request));
-            iFeed = iFeedService.addIFeed(iFeed);
-            model.addAttribute("ifeed", iFeed);
-            response.setRenderParameter("view", "showEditIFeedForm");
-        } else {
-            response.setRenderParameter("view", "showAddIFeedForm");
-        }
-    }
+	@ActionMapping(params = { "action=addIFeed" })
+	public void addIFeed(final Model model, @Valid @ModelAttribute(value = "ifeed") IFeed iFeed,
+	        final BindingResult bindingResult, final ActionRequest request, final ActionResponse response) {
+		if (!bindingResult.hasErrors()) {
+			iFeed.setUserId(getRemoteUserId(request));
+			iFeed = iFeedService.addIFeed(iFeed);
+			try {
+				ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+				long companyId = themeDisplay.getCompanyId();
+				long userId = themeDisplay.getUserId();
+				resourceLocalService.addResources(companyId, 0, userId, IFeed.class.getName(), iFeed.getId()
+				        .longValue(), false, false, true);
+			} catch (PortalException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SystemException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			model.addAttribute("ifeed", iFeed);
+			response.setRenderParameter("view", "showEditIFeedForm");
+		} else {
+			response.setRenderParameter("view", "showAddIFeedForm");
+		}
+	}
 
-    private String getRemoteUserId(PortletRequest request) {
-        @SuppressWarnings("unchecked")
-        Map<String, ?> userInfo = (Map<String, ?>) request.getAttribute(
-                PortletRequest.USER_INFO);
-        String userId = "";
-        if (userInfo != null) {
-            userId = (String) userInfo.get(
-                    PortletRequest.P3PUserInfos.USER_LOGIN_ID.toString());
-        }
-        return userId;
-    }
+	private String getRemoteUserId(PortletRequest request) {
+		@SuppressWarnings("unchecked")
+		Map<String, ?> userInfo = (Map<String, ?>) request.getAttribute(PortletRequest.USER_INFO);
+		String userId = "";
+		if (userInfo != null) {
+			userId = (String) userInfo.get(PortletRequest.P3PUserInfos.USER_LOGIN_ID.toString());
+		}
+		return userId;
+	}
 }

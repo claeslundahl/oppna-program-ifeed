61ad7f1898fb9e0d1bca96a15496c9b1d04dfd54
claes.lundahl@gmail.com <claes.lundahl@gmail.com@9c261296-49e3-ae38-c888-6b3422c0b4e0>
2011-11-15 20:11:56 +0000

    Test for ShowDocumentMetadataController.

---
+++ core-bc/modules/portlet/src/test/java/se/vgregion/ifeed/controller/ShowDocumentMetadataControllerTest.java
@@ -0,0 +1,120 @@
+package se.vgregion.ifeed.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.portlet.PortletResponse;
+import javax.portlet.ResourceResponse;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.MessageSource;
+import org.springframework.ui.Model;
+import org.springframework.web.portlet.ModelAndView;
+
+import se.vgregion.ifeed.service.alfresco.store.AlfrescoDocumentService;
+import se.vgregion.ifeed.service.alfresco.store.DocumentInfo;
+import se.vgregion.ifeed.service.exceptions.IFeedServiceException;
+
+public class ShowDocumentMetadataControllerTest {
+
+	ShowDocumentMetadataController sdmc;
+	ResourceResponse resourceResponse;
+	AlfrescoDocumentService ads;
+
+	@Before
+	public void setUp() throws Exception {
+		ads = mock(AlfrescoDocumentService.class);
+		sdmc = new ShowDocumentMetadataController(ads);
+		resourceResponse = mock(ResourceResponse.class);
+	}
+
+	@Test
+	public void showTopDocumentMetadata() throws IOException {
+		sdmc.setTopListMetadata(new ArrayList<String>());
+		OutputStream os = mock(OutputStream.class);
+		when(resourceResponse.getPortletOutputStream()).thenReturn(os);
+
+		final Map<String, String> getMetdataValuesResult = new HashMap<String, String>();
+		final ShowDocumentMetadataController sdmc;
+
+		sdmc = new ShowDocumentMetadataController(ads) {
+			@Override
+			protected Map<String, String> getMetdataValues(String documentId) {
+				assertEquals("abc", documentId);
+				getMetdataValuesResult.put("key", "value");
+				return getMetdataValuesResult;
+			}
+
+			@Override
+			protected void sendJsonResponse(ResourceResponse response, Map<String, String> metadataPair) {
+				assertEquals(getMetdataValuesResult, metadataPair);
+				assertEquals(resourceResponse, response);
+			}
+		};
+
+		sdmc.showTopDocumentMetadata("abc", resourceResponse);
+	}
+
+	@Test
+	public void getMetdataValuesError() {
+		sdmc.setTopListMetadata(Arrays.asList("key"));
+		DocumentInfo documentInfo = mock(DocumentInfo.class);
+		when(documentInfo.getMetadataValue("key", Object.class)).thenReturn("value");
+		when(ads.getDocumentInfo("docId")).thenThrow(new IFeedServiceException("Title", "Explaination"));
+
+		Map<String, String> result = sdmc.getMetdataValues("docId");
+		assertTrue(result.containsKey("Varning"));
+		assertTrue(result.containsValue("Ingen metadata kunde hittas!"));
+
+	}
+
+	@Test
+	public void getMetdataValues() {
+		sdmc.setTopListMetadata(Arrays.asList("key"));
+		DocumentInfo documentInfo = mock(DocumentInfo.class);
+		when(documentInfo.getMetadataValue("key", Object.class)).thenReturn("value");
+		when(ads.getDocumentInfo("docId")).thenReturn(documentInfo);
+
+		MessageSource resource = mock(MessageSource.class);
+		when(resource.getMessage("key.label", null, "key", null)).thenReturn("key1label");
+		sdmc.setResource(resource);
+
+		Map<String, String> map = sdmc.getMetdataValues("docId");
+		assertEquals("key1label", map.keySet().iterator().next());
+		assertEquals("value", map.values().iterator().next());
+	}
+
+	@Test
+	public void showDocumentMetadata() {
+		Model model = mock(Model.class);
+		String result = sdmc.showDocumentMetadata(model, "docId");
+		assertEquals("documentMetadata", result);
+	}
+
+	@Test
+	public void handleAlfrescoDocumentServiceException() {
+		Exception e = new IFeedServiceException("key", "defaultMessage");
+		handleAlfrescoDocumentServiceException(e, e);
+		e = new RuntimeException(e);
+		handleAlfrescoDocumentServiceException(e, new IFeedServiceException("error.unhandled",
+		        "Internal IFeed Error", e));
+	}
+
+	public void handleAlfrescoDocumentServiceException(Exception e, Exception expexted) {
+		PortletResponse response = mock(PortletResponse.class);
+		ModelAndView result = sdmc.handleAlfrescoDocumentServiceException(e, response);
+		assertEquals(expexted.getMessage(), ((Exception) result.getModel().get("exception")).getMessage());
+		assertEquals("ExceptionHandler", result.getViewName());
+	}
+
+}

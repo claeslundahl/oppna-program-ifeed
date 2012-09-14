f9aaf59f76fede614286152a377016ce3c0a6168
claes.lundahl@gmail.com <claes.lundahl@gmail.com@9c261296-49e3-ae38-c888-6b3422c0b4e0>
2012-09-14 07:36:42 +0000

    [maven-release-plugin]  copy for tag iFeed-system-1.7

---
+++ tags/iFeed-system-1.7/core-bc/modules/intsvc/src/test/java/se/vgregion/ifeed/intsvc/atom/IFeedFeedServiceImplTest.java
@@ -0,0 +1,183 @@
+package se.vgregion.ifeed.intsvc.atom;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.UriInfo;
+import javax.xml.namespace.QName;
+
+import org.apache.abdera.model.Element;
+import org.apache.abdera.model.Entry;
+import org.apache.abdera.model.Feed;
+import org.junit.Before;
+import org.junit.Test;
+
+import se.vgregion.ifeed.service.ifeed.IFeedService;
+import se.vgregion.ifeed.service.solr.IFeedSolrQuery;
+import se.vgregion.ifeed.types.IFeed;
+
+public class IFeedFeedServiceImplTest {
+
+    IFeedFeedServiceImpl serv;
+    private IFeedSolrQuery solrQuery;
+    private IFeedService iFeedService;
+    UriInfo context;
+
+    @Before
+    public void setUp() throws Exception {
+        iFeedService = mock(IFeedService.class);
+        context = mock(UriInfo.class);
+        solrQuery = mock(IFeedSolrQuery.class);
+        serv = new IFeedFeedServiceImpl(solrQuery, iFeedService);
+
+        URI uri = new URI("absolutePath");
+        when(context.getAbsolutePath()).thenReturn(uri);
+        serv.setContext(context);
+    }
+
+    @Test
+    public void getIFeed() throws URISyntaxException {
+        Long id = 100l;
+        String sortField = "sortField";
+        String sortDirection = "asc";
+
+        IFeed feed = mock(IFeed.class);
+        when(feed.getName()).thenReturn("feedName");
+        when(iFeedService.getIFeed(id)).thenReturn(feed);
+
+        Feed result = serv.getIFeed(id, sortField, sortDirection);
+        assertEquals(feed.getName(), result.getTitle());
+    }
+
+    @Test
+    public void getIFeedEntry_error_on_no_result() {
+        try {
+            Entry result = serv.getIFeedEntry(100l);
+            fail();
+        } catch (WebApplicationException e) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void getIFeedEntry() {
+        long id = 100l;
+        IFeed iFeed = new IFeed();
+        when(iFeedService.getIFeed(id)).thenReturn(iFeed);
+        iFeed.setName("feedName");
+
+        Entry result = serv.getIFeedEntry(id);
+        assertEquals(iFeed.getName(), result.getTitle());
+    }
+
+    @Test
+    public void populateEntry_checked_default_values() {
+        Entry entry = mock(Entry.class);
+        Map<String, Object> map = new HashMap<String, Object>();
+        Entry result = serv.populateEntry(entry, map);
+
+        verify(entry).setTitle(any(String.class));
+        verify(entry).addAuthor(any(String.class));
+        verify(entry).setUpdated(any(Date.class));
+    }
+
+    @Test
+    public void populateEntry_non_default_values() {
+        Entry entry = mock(Entry.class);
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        String title = "t", author = "a", summary = "s";
+        Date updated = new Date(1000l);
+
+        map.put("dc.creator", author);
+        map.put("processingtime", updated);
+        map.put("dc.title", title);
+        map.put("dc.description", summary);
+
+        Entry result = serv.populateEntry(entry, map);
+
+        map.put("url", "http://acme.com");
+        map.put("dc.format", "html");
+
+        List<Integer> ints = Arrays.asList(1, 2, 3);
+        map.put("ints.0", ints);
+
+        verify(entry).setTitle(eq(title));
+        verify(entry).addAuthor(eq(author));
+        verify(entry).setUpdated(eq(updated));
+        verify(entry).setSummary(eq(summary));
+
+        assertEquals(result, entry);
+    }
+
+    @Test
+    public void addElementDate() {
+        String prefix = "prefix";
+        String fieldName = "fieldName";
+        Map<String, String> ns = new HashMap<String, String>();
+        ns.put("prefix", "prefix0");
+        addElement(prefix, fieldName, ns, new Date(0l));
+    }
+
+    @Test
+    public void addElementLanguages() {
+        String fieldName = "dc.language";
+        // Map<String, String> ns = new HashMap<String, String>();
+        // ns.put("prefix", "prefix0");
+
+        Map<String, String> prefixes = new HashMap<String, String>();
+        prefixes.put("svenska", "swe");
+        prefixes.put("norska", "nor");
+        prefixes.put("engelska", "eng");
+        serv.setNamespaces(prefixes);
+
+        for (Map.Entry<String, String> prefix : prefixes.entrySet()) {
+            Element element = addElement(prefix.getKey(), fieldName, prefixes, prefix.getKey());
+            verify(element).setText(eq(prefix.getValue()));
+        }
+    }
+
+    public Element addElement(String prefix, String fieldName, Map<String, String> ns, Object value) {
+        Entry e = mock(Entry.class);
+        Element element = mock(Element.class);
+
+        when(e.addExtension(new QName(ns.get(prefix), fieldName.substring(prefix.length() + 1), prefix)))
+                .thenReturn(element);
+
+        serv.setNamespaces(ns);
+        serv.addElement(e, prefix, fieldName, value);
+
+        return element;
+    }
+
+    @Test
+    public void populateFeed() {
+        Feed f = mock(Feed.class);
+        Entry entry = mock(Entry.class);
+        when(f.addEntry()).thenReturn(entry);
+        Collection<Map<String, Object>> hits = new ArrayList<Map<String, Object>>();
+        Map<String, Object> item = new HashMap<String, Object>();
+        item.put("key", "value");
+        hits.add(item);
+
+        serv.populateFeed(f, hits);
+    }
+
+}
---
+++ tags/iFeed-system-1.7/core-bc/modules/portlet/src/test/java/se/vgregion/ifeed/controller/IFeedControllerTest.java
@@ -0,0 +1,171 @@
+package se.vgregion.ifeed.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.portlet.RenderRequest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.ui.Model;
+import org.springframework.web.util.UriTemplate;
+
+import se.vgregion.ifeed.formbean.IFeedFormBeanList;
+import se.vgregion.ifeed.service.ifeed.IFeedService;
+import se.vgregion.ifeed.service.solr.IFeedSolrQuery.SortDirection;
+import se.vgregion.ifeed.types.IFeed;
+
+public class IFeedControllerTest {
+
+	IFeedService iFeedService;
+	UriTemplate iFeedAtomFeed;
+
+	@Before
+	public void setUp() throws Exception {
+		iFeedService = mock(IFeedService.class);
+		iFeedAtomFeed = mock(UriTemplate.class);
+	}
+
+	@Test
+	public void showIFeeds() {
+		showIFeeds(true);
+		showIFeeds(false);
+	}
+
+	public void showIFeeds(boolean forUser) {
+		IFeedController controller = mkControllerNoHandlersPopulate();
+
+		Model model = mock(Model.class);
+		if (forUser) {
+			when(model.asMap()).thenReturn(Collections.singletonMap("currentView", (Object) "viewMine"));
+		}
+		int delta = 1;
+		int cur = 2;
+		String orderByCol = null;
+		SortDirection orderByType = null;
+		RenderRequest request = mock(RenderRequest.class);
+
+		String result = controller.showIFeeds(model, delta, cur, orderByCol, orderByType, request);
+
+		if (forUser) {
+			verify(iFeedService, times(1)).getUserIFeeds(anyString());
+		} else {
+			assertEquals("index", result);
+		}
+	}
+
+	@Test
+	public void handleViewPagination() {
+		IFeedController controller = new IFeedController(iFeedService, iFeedAtomFeed);
+		List<IFeed> viewList = new ArrayList<IFeed>();
+		for (int i = 0; i < 105; i++) {
+			IFeed feed = new IFeed();
+			feed.setId((long) i);
+			viewList.add(feed);
+		}
+
+		List<IFeed> result = controller.handleViewPagination(viewList, 10, 1);
+		assertEquals(10, result.size());
+		assertEquals(viewList.subList(0, 10), result);
+	}
+
+	@Test
+	public void showUserIFeeds() {
+		IFeedController controller = mkControllerNoHandlersPopulate();
+		Model model = mock(Model.class);
+		int delta = 10;
+		int cur = 1;
+		String orderByCol = "id";
+		SortDirection orderByType = SortDirection.asc;
+		RenderRequest request = mock(RenderRequest.class);
+
+		String result = controller.showUserIFeeds(model, delta, cur, orderByCol, orderByType, request);
+		assertEquals("index", result);
+
+	}
+
+	@Test
+	public void handleViewSort() {
+		IFeedController controller = new IFeedController(iFeedService, iFeedAtomFeed);
+		List<IFeed> viewList = new ArrayList<IFeed>();
+		String orderByCol = "id";
+		SortDirection orderByType = SortDirection.desc;
+
+		for (long i = 0; i < 10; i++) {
+			IFeed feed = new IFeed();
+			feed.setId(i);
+		}
+
+		List<IFeed> result = controller.handleViewSort(viewList, orderByCol, orderByType);
+		long expextedId = 10;
+		for (IFeed feed : result) {
+			expextedId--;
+			assertEquals(expextedId, feed.getId().longValue());
+		}
+
+	}
+
+	@Test
+	public void showAllIFeeds() {
+		IFeedController controller = mkControllerNoHandlersPopulate();
+		Model model = mock(Model.class);
+		int delta = 10;
+		int cur = 1;
+		String orderByCol = "id";
+		SortDirection orderByType = SortDirection.asc;
+		RenderRequest request = mock(RenderRequest.class);
+		String result = controller.showAllIFeeds(model, delta, cur, orderByCol, orderByType);
+		assertEquals("index", result);
+	}
+
+	@Test
+	public void populateModel() {
+		final IFeedController controller = new IFeedController(iFeedService, iFeedAtomFeed);
+		Model model = mock(Model.class);
+		int delta = 7;
+		List<IFeed> iFeeds = new ArrayList<IFeed>();
+		int numberOfIfeeds = 10;
+		String orderCol = "id";
+		SortDirection orderType = SortDirection.desc;
+		String viewName = "";
+
+		controller.populateModel(model, iFeeds, numberOfIfeeds, delta, orderCol, orderType, viewName);
+
+		verify(model, times(1)).addAttribute(eq("numberOfIfeeds"), eq(numberOfIfeeds));
+		verify(model, times(1)).addAttribute(eq("ifeeds"), eq(new IFeedFormBeanList(iFeeds, iFeedAtomFeed)));
+		verify(model, times(1)).addAttribute(eq("delta"), eq(delta));
+		verify(model, times(1)).addAttribute(eq("orderByCol"), eq(orderCol));
+		verify(model, times(1)).addAttribute(eq("orderByType"), eq(orderType));
+		verify(model, times(1)).addAttribute(eq("toolbarItem"), eq(viewName));
+		verify(model, times(1)).addAttribute(eq("currentView"), eq(viewName));
+	}
+
+	private IFeedController mkControllerNoHandlersPopulate() {
+		final IFeedController controller = new IFeedController(iFeedService, iFeedAtomFeed) {
+			@Override
+			protected List<IFeed> handleViewPagination(List<IFeed> viewList, int delta, int pageNumber) {
+				return new ArrayList<IFeed>(viewList);
+			}
+
+			@Override
+			protected List<IFeed> handleViewSort(List<IFeed> viewList, String orderByCol, SortDirection orderByType) {
+				return new ArrayList<IFeed>(viewList);
+			}
+
+			@Override
+			protected void populateModel(Model model, List<IFeed> iFeeds, int numberOfIfeeds, int delta,
+			        String orderCol, SortDirection orderType, String viewName) {
+			}
+		};
+		return controller;
+	}
+}
